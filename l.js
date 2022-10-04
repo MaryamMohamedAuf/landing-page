0e9faf318f785caa1863acaa5d28f9e777b06e02
MariamMohamedOof <105866229+MariamMohamedOof@users.noreply.github.com>
2022-10-04 18:23:07 +0200

    Add files via upload

---
+++ l.js
@@ -0,0 +1,8 @@
+let img=document.getElementById("img");
+// let cont=document.querySelector(".cont");
+let content=document.querySelector(".h");
+
+function phone(phone){
+img.src=phone;}
+function colors(colors)
+{content.style.color=colors;}
